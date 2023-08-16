de84341ecbbd69a82616ab15c9c9f65df03d75ef
lsminter <lsminteris@gmail.com>
2023-08-16 10:22:03 -0700

    added second image to overlay text

--- javascript-add-image-watermark/src/index.js
+++ javascript-add-image-watermark/src/index.js
@@ -1,13 +1,17 @@
-import { Cloudinary } from "@cloudinary/url-gen";
-import {Transformation} from "@cloudinary/url-gen";
-import {fill} from "@cloudinary/url-gen/actions/resize";
-import {scale} from "@cloudinary/url-gen/actions/resize";
+import {Cloudinary, Transformation} from "@cloudinary/url-gen";
+import {fill, scale} from "@cloudinary/url-gen/actions/resize";
 import {source} from "@cloudinary/url-gen/actions/overlay";
-import {image} from "@cloudinary/url-gen/qualifiers/source";
+import {image, text} from "@cloudinary/url-gen/qualifiers/source";
+import {Position} from "@cloudinary/url-gen/qualifiers/position";
+import {compass} from "@cloudinary/url-gen/qualifiers/gravity";
+import {TextStyle} from "@cloudinary/url-gen/qualifiers/textStyle";
+import { opacity } from "@cloudinary/url-gen/actions/adjust";
+
+
 
 const cld = new Cloudinary({
   cloud: {
-    cloudName: 'product_environment_key'
+    cloudName: 'dlq9isoe5'
   }
 });
 
@@ -22,8 +26,29 @@ myImage
         .resize(scale().height(55))
         )
     )
+    .position(new Position().gravity(compass('south_east')))
   )
+.format('png')
+
+const mySecondImage = cld.image('cld-sample')
+
+mySecondImage
+.resize(fill(300, 300))
+.overlay(   
+  source(
+    text('PREVIEW', new TextStyle('arial', 60)
+    .fontWeight('bold'))
+    .textColor('gray')
+    .transformation(new Transformation().adjust(opacity(70)))
+  )
+)
+.format('png')
+
 
 const imgElement = document.createElement('img');
 document.body.appendChild(imgElement);
 imgElement.src = myImage.toURL();
+
+const secondImgElement = document.createElement('img');
+document.body.appendChild(secondImgElement);
+secondImgElement.src = mySecondImage.toURL();
